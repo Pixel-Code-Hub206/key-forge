ba120220b0b4c0cdc4fa3fd78b02b9250255cf47
Pixel-Code-Hub206 <mayanklan206@gmail.com>
2025-11-15 19:50:07 +0530

    Initialized a fully functioning PasswordEncoder(And Decoder) to be used with the FileHander. Created FileHandler file to handle(Store and load) passwords from the file.

--- src/Encoder.java
+++ src/Encoder.java
@@ -1,4 +1,13 @@
+import java.util.Base64;
+
 public class Encoder {
 
-    //Placeholder. Encoder class declared for the future as an EndGame Feature.
+    public static String encode(String rawPassword){
+        return Base64.getEncoder().encodeToString(rawPassword.getBytes());      //Returns the String after encoding it to write in the file
+    }
+
+    public static String decode(String encodedPassword){
+        byte[] decodedBytes = Base64.getDecoder().decode(encodedPassword);      //Returns the String formed from the decoded bytes
+        return new String(decodedBytes);
+    }
 }
---
+++ src/FileHandler.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileWriter;
+import java.util.Map;
+
+public class FileHandler {
+
+    private static final String FILE_PATH = "/home/pixel/Documents/saved_passwords.txt";
+
+    public static void loadPasswords()          //Since we are returning maps
+    {
+        //File Handling logic here combined with the encoder
+    }
+}
